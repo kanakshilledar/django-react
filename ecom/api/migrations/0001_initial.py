f8315e2f2fe0e65eb6ff9eeaf0c45369aff1604d
Kanak Shilledar <kanakshilledar111@gmail.com>
2021-05-12 12:04:32 +0530

    Finally the login issue is removed!!! :)

--- ecom/api/migrations/0001_initial.py
+++ ecom/api/migrations/0001_initial.py
@@ -1,14 +1,23 @@
 from django.db import migrations
 from api.user.models import CustomUser
 
-class Migrations(migrations.Migration):
+
+class Migration(migrations.Migration):
     def seed_data(apps, schema_editor):
-        user = CustomUser(name="kanak", email="kanak@kanak.io", is_staff=True, is_superuser=True, phone="987654321", gender="M",)
+        user = CustomUser(name="kanak",
+                          email="kanak@kanak.io",
+                          is_staff=True,
+                          is_superuser=True,
+                          phone="987654321",
+                          gender="Male"
+                          )
+        user.set_password("12345")
+        user.save()
 
-    user.set_password("kanak")
-    user.save()
+    dependencies = [
 
-    dependencies = []
+    ]
+
     operations = [
         migrations.RunPython(seed_data),
     ]
--- ecom/api/user/serializers.py
+++ ecom/api/user/serializers.py
@@ -1,16 +1,18 @@
 from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
 from rest_framework.decorators import authentication_classes, permission_classes
+
 from .models import CustomUser
 
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+
     def create(self, validated_data):
         password = validated_data.pop('password', None)
         instance = self.Meta.model(**validated_data)
-        
+
         if password is not None:
             instance.set_password(password)
-        
         instance.save()
         return instance
 
@@ -19,12 +21,13 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
             if attr == 'password':
                 instance.set_password(value)
             else:
-                setattr(instance, attr, value)
-                
+                setattr(instace, attr, value)
+
         instance.save()
         return instance
 
     class Meta:
         model = CustomUser
         extra_kwargs = {'password': {'write_only': True}}
-        fields = ('name', 'email', 'password', 'phone', 'gender', 'is_active', 'is_staff', 'is_superuser')
+        fields = ('name', 'email', 'password', 'phone', 'gender',
+                  'is_active', 'is_staff', 'is_superuser')
